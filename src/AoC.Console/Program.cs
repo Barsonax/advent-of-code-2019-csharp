b0689589bd9dc7423cc88f3d040dd67872f03669
Rick van Dam <rick.van.dam@avivasolutions.nl>
2019-12-19 17:55:25 +0100

    wip

---
+++ src/AoC.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace AoC.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("Day1/input.txt");
+            Day1.Calculate(input);
+        }
+    }
+}
--- src/AoC/Class1.cs
+++ src/AoC/Class1.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Linq;
 
 namespace AoC
 {
-    public class Class1
+    public static class Day1
     {
+        public static void Calculate(string input)
+        {
+            var modules = input.Split(new[] { Environment.NewLine }, StringSplitOptions.None).Select(int.Parse).ToArray();
+            var result = modules.Sum(x => Math.Floor(x / 3f) - 2);
+        }
     }
 }
